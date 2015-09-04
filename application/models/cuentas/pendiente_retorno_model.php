5c9f6c6d840d47111478d9881680a68bff6cdc12
Omar Caballero <omar.caballero@aventuracp.com.mx>
2015-09-03 19:37:27 -0500

    falta que entre la funcion de suma de pagos para pagos de un solo folio

--- application/models/cuentas/pendiente_retorno_model.php
+++ application/models/cuentas/pendiente_retorno_model.php
@@ -16,4 +16,25 @@ class Pendiente_retorno_model extends CI_Model
 		$query = $this->db->get();
 		return $query->result();
 	}
+
+	public function get_in_query($table, $array, $camp)
+	{
+		$this->db->from($table);
+		$this->db->where_in($camp, $array);
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	public function get_all_query($table)
+	{
+		$query = $this->db->get($table);
+		return $query->result();
+	}
+
+	public function update_where_query($table,  $data_up, $array_where )
+	{
+		$this->db->where($array_where);
+		$this->db->update($table, $data_up);
+		
+	}
 }
--- application/views/admin/cuentas/pendiente_retorno/modales_depositos.php
+++ application/views/admin/cuentas/pendiente_retorno/modales_depositos.php
@@ -114,7 +114,41 @@ function pagos(id_empresa, id_banco, id_deposito)
                         <div class="form-group">
                             <label class="control-label col-sm-4"> Monto </label>
                             <div class="col-sm-8">
-                                <input type="text" name="monto">
+                                <input type="text" name="monto" id="monto">
+                                <label class="text-danger" id="fail_monto" style="display:none">*Este campo es requerido</label>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="control-label col-sm-4 col-xs-4"> Empresa retorno</label>
+                            <div class="col-sm-8 col-xs-8">
+                                    <select class="form-control" name="empresa_retorno" id="empresa_retorno"  >
+                                        <option value=""> Seleccione un cliente</option>
+                                        
+                                    </select>
+                               <label class="text-danger" id="fail_empresa_retorno" style="display:none">*Este campo es requerido</label>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group" id="list_banks">
+                            <label class="control-label col-sm-4 col-xs-4"> Banco</label>
+                            <div class="col-sm-8 col-xs-8">
+                                <select class="form-control" name="id_banco_option" id="id_banco_option" > 
+                                    <option value = "">Seleccione un banco</option>
+                                </select>
+                                <label class="text-danger" id="fail_banco_retorno" style="display:none">*Este campo es requerido</label>
+                            </div>
+                        </div>
+
+                        <div class="form-group" id="folio_pago">
+                            <label class="control-label col-sm-4 col-xs-4"> Folio</label>
+                            <div class="col-sm-8 col-xs-8">
+                                <div class="col-sm-5 col-xs-5">
+                                   <input type="text" class="form-control" name="folio_pago" value="<?=set_value('folio_pago')?>" >
+                                </div>
+                                <div class="col-sm-12 col-xs-12">&nbsp;</div>
+                                <div class="col-sm-4 col-xs-4"><?=form_error('folio_pago')?></div>
                             </div>
                         </div>
 
@@ -124,8 +158,10 @@ function pagos(id_empresa, id_banco, id_deposito)
                                 <input class="form-control date-picker input-large" id="id-date-picker-1" name="fecha_pago" required type="text" data-date-format="dd-mm-yyyy" value="<?=set_value('fecha_pago')?>"  placeholder="dd/mm/aaaa"/>
                                 <span class="input-group-addon">
                                 <i class="icon-calendar bigger-110"></i>
-                                </span>
+                                </span>                                
                             </div>
+                            <label class="control-label col-sm-4 col-xs-4">&nbsp;</label>
+                            <label class="text-danger" id="fail_fecha" style="display:none">*Este campo es requerido</label>
                         </div>
 
                         <div class="form-group">
@@ -135,6 +171,8 @@ function pagos(id_empresa, id_banco, id_deposito)
                                   <input type="hidden" name="ruta_comprobante" id="ruta_comprobante" value="<?=set_value('ruta_comprobante')?>" >
                                   <div id="upload_comprobante" >Clic para cargar</div>
                                   El formato de la imagen debe ser PDF, PNG o JPG con un tamaño maximo de 4 MB.
+                                  <br>
+                                  <label class="text-danger" id="fail_archivo" style="display:none">*Este campo es requerido</label>
                               </div>
                               <div class="col-sm-12 col-xs-12">&nbsp;</div>
                               <div class="col-sm-6 col-xs-6"><?=form_error('ruta_comprobante')?></div>
@@ -159,11 +197,11 @@ function pagos(id_empresa, id_banco, id_deposito)
                             onComplete: function  (id, fileName, responseJSON){
                             if(responseJSON.error == null) {
                             $("#ruta_comprobante").val(responseJSON.directory);
-                            $("#upload_comprobante .qq-uploader .qq-upload-list").html('<li><div class="alert alert-success">Archivo cargado exitosamente.<a href="<?php print base_url();?>'+responseJSON.directory+
+                            $("#upload_comprobante .qq-uploader .qq-upload-list").html('<li><div class="alert alert-success" id="success_file">Archivo cargado exitosamente.<a href="<?php print base_url();?>'+responseJSON.directory+
                             '" target="_blank"> Ver archivo cargado </div></a></li>');
                             } else {
                             $("#ruta_comprobante").val('');
-                            $("#upload_comprobante .qq-uploader .qq-upload-list").html('<li><div class="alert alert-danger">'+responseJSON.error+'</div></li>');
+                            $("#upload_comprobante .qq-uploader .qq-upload-list").html('<li><div class="alert alert-danger" id="fail_file">'+responseJSON.error+'</div></li>');
                             }
                             },
                             onCancel: function(id, fileName){
@@ -172,8 +210,10 @@ function pagos(id_empresa, id_banco, id_deposito)
                               debug: false
                             });
                         </script>
-
-                        <table border="1">
+                        <div class="row text-center text-danger" style="display:none" id="fail_depositos_array">
+                            *Debe seleccionar al menos un depósito
+                        </div>
+                        <table class="table table-striped table-bordered table-hover no-margin-bottom no-border-top">
                             <thead>
                                 <tr>
                                     <th></th>
@@ -185,8 +225,6 @@ function pagos(id_empresa, id_banco, id_deposito)
                             </thead>
                             <tbody id="pendientes_retornar"></tbody>
                         </table>
-
-                        <input type="text" value="" id="prueba">
                     </form>
                 </div>   
 
@@ -209,6 +247,7 @@ function pagos(id_empresa, id_banco, id_deposito)
 </div>
 
 <script type="text/javascript">
+    
 
     $("#show-add-pay").click(function(){
         $("#add-pay").show();
@@ -222,7 +261,7 @@ function pagos(id_empresa, id_banco, id_deposito)
         var deposito = $('#deposito_id').val();
         
 
-        console.log('id_empresa:'+empresa+ ' - id_banco:'+banco+' -empresa:'+deposito);
+       // console.log('id_empresa:'+empresa+ ' - id_banco:'+banco+' -empresa:'+deposito);
 
         $.ajax({
                 type: "POST",
@@ -230,40 +269,223 @@ function pagos(id_empresa, id_banco, id_deposito)
                 url:    "<?=base_url('cuentas/pagos/pending_pay_client')?>",
                 data:   "id_deposito=" +deposito,
                 success: function(data){
-                    console.log(data.length);
+                    //console.log(data.length);
 
                     var html_pendiente = '';
 
                     for(var i=0; i<data.length; i++){
                         html_pendiente += "<tr>";
-                        html_pendiente += "<td>"+data[i].id_deposito+"</td>";
+                        if(data[i].checked == true){
+                            html_pendiente += "<td><input type='checkbox' name='id_deposito_json' value='"+data[i].id_deposito+"' checked=checked ></td>";
+                        }else{
+                            html_pendiente += "<td><input type='checkbox' name='id_deposito_json' value='"+data[i].id_deposito+"' ></td>";
+                        }
+
                         html_pendiente += "<td>"+data[i].folio_deposito+"</td>";
-                        html_pendiente += "<td>"+data[i].monto_deposito+"</td>";
-                        html_pendiente += "<td>"+data[i].comision+"</td>";
-                        html_pendiente += "<td>"+data[i].pendiente_retornar+"</td>";
+                        html_pendiente += "<td>$"+data[i].monto_deposito+"</td>";
+                        html_pendiente += "<td>$"+data[i].comision+"</td>";
+                        html_pendiente += "<td>$"+data[i].pendiente_retornar+"</td>";
                         html_pendiente += "</tr>";             
                     }
 
 
                     $("#pendientes_retornar").html(html_pendiente);
-                    $("#prueba").html('ksmf');
                 }
         });
 
+        $.ajax({
+                type: "POST",
+                dataType: "json",
+                url:    "<?=base_url('cuentas/pagos/empresas')?>",
+                data:   "id_deposito=" +deposito,
+                success: function(data){
+                    
+                    var html_option = '';
+
+                          html_option += '<option value="">Seleccione una empresa</option>';
+                    for(var i=0; i<data.length; i++){
+                        html_option += '<option value="'+data[i].id_empresa+'">'+data[i].name_empresa+'</option>';
+                    }
+                    $('#empresa_retorno').html(html_option);
+
+                }
+        });
+    /////
     });
 
+    $('#empresa_retorno').change(function(){
+        var empresa = $('#empresa_retorno').val();
+        if(empresa !=15){
+            $('#list_banks').show();
+            $('#folio_pago').show();
+            $.ajax({
+                    type: "POST",
+                    datatype: 'html',
+                    url: '<?php echo base_url("cuentas/depositos/bancos_empresa")?>',
+                    data: "id_empresa=" + empresa , 
+                    success: function(data)
+                    {
+                         $("#id_banco_option").html(data);
+                    }
+                  });//fin accion ajax
+        }else{
+            $('#list_banks').hide();
+            $('#folio_pago').hide();
+        }
+    });
+
+    function validate_unique_folio(folio)
+    {
+        $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: '<?php echo base_url("cuentas/pagos/pay_bills")?>',
+                data: "folio=" + folio,
+                success: function(data)
+                {   
+                    return data;
+
+                }
+              });//fin accion ajax
+    }
+    
+    
+
     $("#pay-bill").click(function(){
-        $("#add-pay").hide();
-        $("#button-add-pay").hide();
+
+        if( $("#monto").val() == '' || $("#monto").val() == undefined ){
+            console.log('primero ',$("#monto").val());
+            $('#fail_monto').show();
+            console.log('falta monto');
+            return false;
+        }
+
+        if( $("#empresa_retorno").val() == '' || $("#empresa_retorno").val() == undefined  ){
+            $('#fail_monto').hide();
+            $('#fail_fecha').hide();
+            $('#fail_archivo').hide();
+            $('#fail_empresa_retorno').show();
+
+            console.log('tercero ',$("#empresa_retorno").val());
+            console.log('falta empresa retorno');
+            return false;
+        }else{
+            if($("#empresa_retorno").val() != 15)
+            {
+                // if( $("#id_banco_option").val() == '' || $("#id_banco_option").val() == undefined  ){
+                //     $('#fail_monto').hide();
+                //     $('#fail_fecha').hide();
+                //     $('#fail_archivo').hide();
+                //     $('#fail_empresa_retorno').hide();
+                //     $('#fail_banco_retorno').show();
+
+                    
+
+                //     console.log('tercero ',$("#id_banco_option").val());
+                //     console.log('falta banco retorno');
+                //     return false;
+                // }
+
+                // if( $("#folio_pago").val() == '' || $("#folio_pago").val() == undefined  ){
+                    
+                //     $('#fail_monto').hide();
+                //     $('#fail_fecha').hide();
+                //     $('#fail_archivo').hide();
+                //     $('#fail_empresa_retorno').hide();
+                //     $('#fail_banco_retorno').show();
+
+                    
+
+                //     console.log('tercero ',$("#folio_pago").val());
+                //     console.log('falta banco retorno');
+                //     return false;
+                // }else{
+                //     //var unique = validate_unique_folio($('#folio_pago').val());
+
+                // }
+            }
+
+        }
+
+
+
+
+
+        if( $("input:text[name=fecha_pago]").val() == '' || $("input:text[name=fecha_pago]").val() == undefined  ){
+            $('#fail_monto').hide();
+            $('#fail_fecha').show();
+
+            console.log('segundo ',$("input:text[name=fecha_pago]").val());
+            console.log('falta fecha');
+            return false;
+        }
+
+        if( $("#ruta_comprobante").val() == '' || $("#ruta_comprobante").val() == undefined  ){
+            $('#fail_monto').hide();
+            $('#fail_fecha').hide();
+            $('#fail_archivo').show();
+
+            console.log('tercero ',$("#ruta_comprobante").val());
+            console.log('falta comprobante');
+            return false;
+        }
+
         
-        $("#table-pays").show();
-        $("#button-list-pays").show();
+
+
+        var type = [];
+        $("input[name=id_deposito_json]:checked").each(function() {type.push(this.value)});
+
+        console.log(type.length);
+        if(type.length == 0){
+            $('#fail_depositos_array').show();
+            return false;
+        }
+
+        $('#fail_monto').hide();
+        $('#fail_fecha').hide();
+        $('#fail_archivo').hide();
+        $('#fail_depositos').show();
+        $('#success_file').hide();
+        $('#fail_file').hide();
+        $('#fail_depositos_array').hide();
+        $('#fail_banco_retorno').hide();
+        $('#fail_empresa_retorno').hide();
+
+        
+
+        
 
         var empresa = $('#id_empresa').val();
         var banco   = $('#id_banco').val();
         var deposito = $('#deposito_id').val();
 
-        console.log('--id_empresa:'+empresa+ ' - id_banco:'+banco+' -empresa:'+deposito);
+        //console.log('--id_empresa:'+empresa+ ' - id_banco:'+banco+' -empresa:'+deposito);
+
+        $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: '<?php echo base_url("cuentas/pagos/pay_bills")?>',
+                data: "fecha_pago=" + $("input:text[name=fecha_pago]").val() + "&comprobante="+ $("#ruta_comprobante").val()  + "&id_depositos=" + type+"&monto_pago="+$("#monto").val() + "&empresa_retorno="+$("#empresa_retorno").val()+"&banco_retorno="+$("#id_banco_option").val(), 
+                success: function(data)
+                {   
+                    pagos(empresa, banco, deposito);
+
+                    $('#monto').val('');
+                    $("input:text[name=fecha_pago]").val('');
+                    $('#ruta_comprobante').val('');
+
+
+                    $("#add-pay").hide();
+                    $("#button-add-pay").hide();
+                    
+                    $("#table-pays").show();
+                    $("#button-list-pays").show();
+
+
+                }
+              });//fin accion ajax
+        
 
     });
 
