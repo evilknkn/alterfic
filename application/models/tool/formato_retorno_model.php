aae83dc5a921faa5d671522eb4b329444ff336f1
Omar Caballero <omar.caballero@aventuracp.com.mx>
2015-12-15 18:55:04 -0600

    inicio de formato query para crear tabla

--- application/models/tool/formato_retorno_model.php
+++ application/models/tool/formato_retorno_model.php
@@ -48,3 +48,16 @@ class Formato_retorno_model extends CI_Model
 		return $query->row();
 	}
 }
+/*
+CREATE TABLE `ad_formato_retorno` (
+  `id_formato` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `folio_cliente` varchar(50) DEFAULT NULL,
+  `id_deposito` int(11) DEFAULT NULL,
+  `tipo_retorno` varchar(10) DEFAULT NULL,
+  `nombe` varchar(250) DEFAULT NULL,
+  `monto` float(10,2) DEFAULT NULL,
+  `parametro` varchar(100) DEFAULT NULL,
+  `reg_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_formato`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+*/
